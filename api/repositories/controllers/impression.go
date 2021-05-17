40466547e1a2fd0c05e316051251df2956b7db23
Aki Unleash <aki.unleash@gmai.comm>
2021-05-17 16:56:13 +0900

    Completion a ImpressionsRead

--- api/repositories/controllers/impression.go
+++ api/repositories/controllers/impression.go
@@ -49,26 +49,27 @@ func ImpressionsWrite(c echo.Context) error {
 
 }
 
-// Login
+// ImpressionsRead
 // @Summary Processing to display diary
 // @Description Can be executed only at login.
 // @tags diary
 // @Accept  json
 // @Produce  json
 // @Success 200 {object} models.Impression
+// @failure 401 {string} string	"401 unauthenticated"
 // @Router /diary [Get]
 func ImpressionsRead(c echo.Context) error {
 
 	// CookieからUIDを取得
 	uid, err := middle.CurrentUserUid(c)
 	if err != nil {
-		return err
+		return result.Json(c, http.StatusUnauthorized, "401 unauthenticated")
 	}
 
-	var diary []models.Impression
-	database.DB.Where("uid = ?", uid).Order("created_at DESC").Find(&diary)
+	var Impression []models.Impression
+	database.DB.Where("uid = ?", uid).Order("created_at DESC").Find(&Impression)
 
-	return c.JSON(http.StatusOK, diary)
+	return c.JSON(http.StatusOK, Impression)
 }
 
 // Login
